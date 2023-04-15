b27c922c96d1ebc00fdcf16bdd5f13d44ac4daa5
renalex <aleksandr@codetime.am>
2023-04-16 01:17:19 +0400

    First method

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
 <template lang="pug">
-HomeWelcome
-Counter
-  //  <NuxtWelcome /> 
+button(@click="toggle") Toggle
+div(v-if="MyComponent === 'welcome'")
+  HomeWelcome
+Counter(v-else)
+//- HomeWelcome
+//button(@click="toggle") Toggle
+//component(:is="MyComponent")
+//- Counter 
+//  <NuxtWelcome /> 
+
+
+
+
 </template>
+
+<script setup lang="ts">
+// const MyComponent = shallowRef(resolveComponent('Counter'))
+// const toggle = () => MyComponent.value = resolveComponent('HomeWelcome')
+
+const MyComponent = ref('counter')
+const toggle = () => MyComponent.value = 'welcome'
+</script> 
