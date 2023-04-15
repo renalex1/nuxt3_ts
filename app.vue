7e87341aaac6789b3fdce3660105ceb8568aa2f9
renalex <aleksandr@codetime.am>
2023-04-16 01:30:26 +0400

    finish components part whit several mode "Lazy and ..."

--- app.vue
+++ app.vue
@@ -1,8 +1,15 @@
 <template lang="pug">
-button(@click="toggle") Toggle
-div(v-if="MyComponent === 'welcome'")
-  HomeWelcome
-Counter(v-else)
+a(href="#" @click="view('home')") Home
+a(href="#" @click="view('counter')") Counter
+//- HomeWelcome(v-if="page ==='home'")
+//- Counter(v-if="page ==='counter'")
+LazyHomeWelcome(v-if="page ==='home'")
+LazyCounter(v-if="page ==='counter'")
+
+//- button(@click="toggle") Toggle
+//- div(v-if="MyComponent === 'welcome'")
+//-   HomeWelcome
+//- Counter(v-else)
 //- HomeWelcome
 //button(@click="toggle") Toggle
 //component(:is="MyComponent")
@@ -15,9 +22,13 @@ Counter(v-else)
 </template>
 
 <script setup lang="ts">
+const page = ref('home')
+const view = (name: string) => page.value = name
+
+
 // const MyComponent = shallowRef(resolveComponent('Counter'))
 // const toggle = () => MyComponent.value = resolveComponent('HomeWelcome')
 
-const MyComponent = ref('counter')
-const toggle = () => MyComponent.value = 'welcome'
+// const MyComponent = ref('counter')
+// const toggle = () => MyComponent.value = 'welcome'
 </script> 
