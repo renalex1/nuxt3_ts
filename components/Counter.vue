7ecf36382904e7a8e6169231848a81a9e3d25e33
renalex <aleksandr@codetime.am>
2023-04-15 23:55:15 +0400

    Use component

---
+++ components/Counter.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+button(@click="increment") Increment
+p {{ count }}
+button(@click="decrement") Decrement
+  |
+  //  <NuxtWelcome /> 
+</template>
+
+
+<!-- Composition API -->
+
+<script setup lang="ts">
+const count = ref(0);
+
+const increment =()=> count.value++
+const decrement =()=> count.value--
+</script>
+
+<!-- Optional API
+<script  lang="ts">
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    },
+  },
+};
+</script> -->
