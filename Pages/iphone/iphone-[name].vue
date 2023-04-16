c6602df36ff797b098256a150a089f62e291f249
renalex <aleksandr@codetime.am>
2023-04-16 20:32:44 +0400

    Finish meta title ...

--- Pages/iphone/iphone-[name].vue
+++ Pages/iphone/iphone-[name].vue
@@ -1,9 +1,18 @@
 <template lang="pug">
+//- div
+//-   Head
+//-     Title Nuxt 3 - Iphone {{ name }}
 p I am iphone {{ name }}
 </template>
 
 <script setup lang="ts">
+import { Head } from '~/.nuxt/components';
+
 const route = useRoute()
 
 const name = computed(() => route.params.name.toString().replaceAll('-', ' '))
+
+useHead({
+  title:`Nuxt3 - Iphone ${route.params.name.toString().replaceAll('-', ' ')}`
+})
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['~/assets/scss/main.scss'],
+  app: {
+    head: {
+      title: 'Nuxt3',
+    }
+  },
   modules: [
   ],
   build: {
     // ...
   },
-  css: ['~/assets/scss/main.scss'],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "@/assets/scss/_colors.scss" as *;'
-        }
-      }
-    }
-  }
-})
+          additionalData: '@use "@/assets/scss/_colors.scss" as *;',
+        },
+      },
+    },
+  },
+});
