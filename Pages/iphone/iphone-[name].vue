54a5451a784d9f70d02bd04e6533b864e18b9db0
renalex <aleksandr@codetime.am>
2023-04-16 14:11:20 +0400

    Finish dynamic routing

---
+++ Pages/iphone/iphone-[name].vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+p I am iphone {{ name }}
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+const name = computed(() => route.params.name.toString().replaceAll('-', ' '))
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+  ],
+  build: {
+    // ...
+  },
   vite: {
     css: {
       preprocessorOptions: {
